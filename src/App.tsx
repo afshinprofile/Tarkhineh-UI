7e4a96872813bad96997a85a8a53c11f4fbd7204
Afshin Ghezeli <a.g.afshin@gmail.com>
2024-08-20 20:29:36 +0430

    test-tsx-and
    Test TypeScript and Tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
 // import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
+
+let sum = (num1: number, num2: number): number | string => {
+  if (num1 && num2) {
+    return "this numbers negetive";
+  }
+  return num1 + num2;
+};
+
+let priceCount: number = sum(23, 442) as number; 
+console.log(priceCount);
 
 function App() {
   
   return (
     <>
-    <div className='bg-emerald-500 text-white'>this hello world for test</div>
+      <div className='bg-emerald-500 text-white'>this hello world for test</div>
     </>
   )
 }
 
-export default App
+export default App;
