7dcdb49eab8756bd240499f1078e7e790e8d847f
Afshin Ghezeli <a.g.afshin@gmail.com>
2024-08-26 23:19:20 +0430

    add-text
    add test text

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   
   return (
     <>
-    <div></div>
+    <div>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti, perspiciatis suscipit itaque similique velit iure consequatur doloribus quos nostrum architecto hic voluptatibus molestias doloremque atque laborum consequuntur maiores harum ducimus aut aperiam, libero voluptatum ut? Qui fugit quas nulla possimus cupiditate ex! Distinctio eum enim, esse accusantium id cum nisi ducimus iusto aperiam quibusdam sunt consequatur minus quia nesciunt atque. Ex beatae pariatur repellat, qui quaerat consequatur corporis, non libero praesentium iusto modi quas accusantium inventore vel. Quod quam ipsum aut optio, deserunt assumenda quisquam eveniet? Dolorem distinctio animi commodi quae reprehenderit? Sit consectetur velit dicta tenetur, necessitatibus et consequuntur.</div>
     </>
   )
 }
